7e75e360954eeb8a7182370ac991f8f8b6868786
xgqfrms <xgqfrms@outlook.com>
2017-01-06 23:11:02 +0800

    ES2015 for of loop

--- Front-End/Angular2/Apps/racing/src/app/app.component.ts
+++ Front-End/Angular2/Apps/racing/src/app/app.component.ts
@@ -9,17 +9,24 @@ export class AppComponent {
     title = 'app works!';
     carParts = [
         {
-        "id": 1,
-        "name":  "super car",
-        "desc": "These tires are the very best",
-        "inStock": 5,
-        "price": 4.99
+            "id": 1,
+            "name":  "super car",
+            "desc": "These tires are the very best",
+            "inStock": 5,
+            "price": 4.99
         },{
-        "id": 2,
-        "name": "Reinforced Shocks",
-        "desc": "Shocks made from kryptonite",
-        "inStock": 4,
-        "price": 9.99
+            "id": 2,
+            "name": "Reinforced Shocks",
+            "desc": "Shocks made from kryptonite",
+            "inStock": 4,
+            "price": 9.99
         }
     ];
+    totalCarParts() {
+        let sum = 0;
+        for (let carPart of this.carParts) {
+            sum += carPart.inStock;
+        }
+        return sum;
+    }
 }
