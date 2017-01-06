2705d18819e271314d78812f21035e3ae7c5eeb6
xgqfrms <xgqfrms@outlook.com>
2017-01-06 22:34:58 +0800

    *ngIf & *ngFor & pipe

--- Front-End/Angular2/Apps/racing/src/app/app.component.ts
+++ Front-End/Angular2/Apps/racing/src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  carParts = {
-      "id": 1,
-      "name":  "super car",
-      "desc": "These tires are the very best",
-      "inStock": 5
-  };
+    title = 'app works!';
+    carParts = [
+        {
+        "id": 1,
+        "name":  "super car",
+        "desc": "These tires are the very best",
+        "inStock": 5
+        },{
+        "id": 2,
+        "name": "Reinforced Shocks",
+        "desc": "Shocks made from kryptonite",
+        "inStock": 4
+        }
+    ];
 }
