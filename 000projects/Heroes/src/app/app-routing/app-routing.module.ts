4b1b293d61b2685f1b833b7329de97e7cf8eef54
xgqfrms <xgqfrms@outlook.com>
2017-01-09 17:40:26 +0800

    hero

--- 000projects/Heroes/src/app/app-routing/app-routing.module.ts
+++ 000projects/Heroes/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,4 @@
 import { NgModule } from '@angular/core';
-/*import { CommonModule } from '@angular/common';
-
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})*/
 
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from '../dashboard/dashboard.component';
