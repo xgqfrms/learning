0988f34bf47b86afc2aff9027537814ed1885efa
xgqfrms <xgqfrms@outlook.com>
2017-01-14 22:36:02 +0800

    progress-indicator

--- JavaScript/progress-indicator/js.js
+++ JavaScript/progress-indicator/js.js
@@ -1,17 +1,17 @@
 /* progress indicator */
 (function () {
   // var pageHeight = document.documentElement.scrollHeight;
-  var article = document.querySelector('.article');
-  var viewportHeight = document.documentElement.clientHeight;
-  var articleHeight = article.getBoundingClientRect().height;
-  var prog = document.querySelector('.progress-indicator');
-  window.addEventListener('scroll', function () {
-    window.requestAnimationFrame(function () {
-      var perc = Math.max(0, Math.min(1, (viewportHeight - article.getBoundingClientRect().top) / articleHeight));
-      updateProgress(perc);
+    let article = document.querySelector('.article'),
+        viewportHeight = document.documentElement.clientHeight,
+        articleHeight = article.getBoundingClientRect().height,
+        prog = document.querySelector('.progress-indicator');
+    window.addEventListener('scroll', function () {
+        window.requestAnimationFrame(function () {
+            var perc = Math.max(0, Math.min(1, (viewportHeight - article.getBoundingClientRect().top) / articleHeight));
+            updateProgress(perc);
+        });
     });
-  });
-  function updateProgress(perc) {
-    prog.style.width = perc * 100 + '%';
-  }
+    function updateProgress(perc) {
+        prog.style.width = perc * 100 + '%';
+    }
 })();
