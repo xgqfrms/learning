ab12a4e315217fbb609ce3e486e11f28ccdc1926
xgqfrms <xgqfrms@outlook.com>
2017-01-29 23:13:29 +0800

    Loading local JSON file

---
+++ JavaScript/Ajax/JSON/Loading local JSON file.md
@@ -0,0 +1,14 @@
+# Loading local JSON file  
+
+
+http://stackoverflow.com/questions/7346563/loading-local-json-file/39571547#39571547
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/Ajax/JSON/loading-local-json.js
@@ -0,0 +1,21 @@
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', 'my_data.json', true); 
+      // Replace 'my_data' with the path to your file
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+          // Required use of an anonymous callback 
+          // as .open() will NOT return a value but simply returns undefined in asynchronous mode
+          callback(xobj.responseText);
+        }
+  };
+  xobj.send(null);  
+}
+
+function init() {
+loadJSON(function(response) {
+  // Parse JSON string into object
+   var actual_JSON = JSON.parse(response);
+ });
+}
