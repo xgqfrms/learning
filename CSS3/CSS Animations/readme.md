8087013a998b67e41e01b72d505a33d36825c1fb
xgqfrms <xgqfrms@outlook.com>
2017-02-14 06:15:08 +0800

    console.log("%c%s", "color: red; background: yellow; font-size: 24px;", "警告！");
    console.log("%c%s", "color: red; background: yellow; font-size: 24px;",
    "警告！");
    
    console.log(args,args,args,args,args,args,args,??? )
    
    这里的用法很像C语言里格式化字符串输出的printf函数，
    log的第一个参数声明第二、第三个参数的作用， %c%s
    log的第二个参数就是样式，%c
    log的第三个参数是要输出的字符串。%s

--- CSS3/CSS Animations/readme.md
+++ CSS3/CSS Animations/readme.md
@@ -238,7 +238,10 @@ console.log(args,args,args,args,args,args,args,??? )
 
 
 
-
+这里的用法很像C语言里格式化字符串输出的printf函数，
+log的第一个参数声明第二、第三个参数的作用， %c%s
+log的第二个参数就是样式，%c
+log的第三个参数是要输出的字符串。%s
 
 
 
---
+++ Google/Chrome/styles-console.js
@@ -0,0 +1,50 @@
+// styles-console
+
+
+
+/*
+
+
+
+http://www.webhek.com/add-styles-console
+
+如何在浏览器控制台(console)里输出彩色样式调试信息
+
+http://www.webhek.com/wordpress/wp-content/uploads/2015/07/c-1-500x456.png
+
+
+
+console.log("%c%s", "color: red; background: yellow; font-size: 24px;", "警告！");
+
+console.log(args,args,args,args,args,args,args,??? )
+
+
+
+这里的用法很像C语言里格式化字符串输出的printf函数，
+log的第一个参数声明第二、第三个参数的作用， %c%s
+log的第二个参数就是样式，%c
+log的第三个参数是要输出的字符串。%s
+
+
+
+
+
+
+
+*/
+
+
+
+
+console.log("%c%s", "color: red; background: yellow; font-size: 24px;", "警告！");
+
+
+
+
+
+
+
+
+
+
+
