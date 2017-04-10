330840649f09a6ed8e4faa8f8ea1f9f475e876fb
anonymous <xgqfrms@gmail.com>
2017-04-10 14:09:33 +0800

    alert("https://www.xgqfrms.xyz/tracker.js");
    (function() {
        alert("https://www.xgqfrms.xyz/tracker.js");
    })();

---
+++ tracker.js
@@ -0,0 +1,18 @@
+// https://gist.github.com/xgqfrms-GitHub/70bede946542a084182db9d952d36b9e#gistcomment-2054115
+
+
+/*
+
+(function() {
+    var bi_hm = document.createElement("script");
+    bi_hm.src = "https://www.xgqfrms.xyz/tracker.js";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bi_hm, s);
+})();
+
+*/
+
+
+(function() {
+    alert("https://www.xgqfrms.xyz/tracker.js");
+})();
