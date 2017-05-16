4bf3553443d556cebc111652f7dd7a63a31f73cd
xgqfrms <xgqfrms@outlook.com>
2017-05-17 01:00:23 +0800

    node.js-async-generator-promise.js
    https://gist.github.com/xgqfrms-GitHub/79f3fcc2e19d1bcbcddb2faa6e362d76

--- Front-End-Tools/Webpack/fetch-api.js
+++ Front-End-Tools/Webpack/fetch-api.js
@@ -2,11 +2,14 @@
  * copyright 2017 xgqfrms
  * 2017.05.16
  * fetch-api
+ * es6/es7
  */
 
 
 
 /*
+
+https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/async_function
 
 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/await
 
@@ -28,14 +31,10 @@ x1 = (param1, ..., paramn) => {
 }
 // Uncaught SyntaxError: Unexpected token ,
 
-
-
-
 x1 = (param1, ... , paramn) => {
     console.log(`%c {statements}`, `color: red;`);
 }
 // Uncaught SyntaxError: Unexpected token ,
-
 
 
 x1 = (param1, ...rest , paramn) => {
@@ -43,12 +42,9 @@ x1 = (param1, ...rest , paramn) => {
 }
 // Uncaught SyntaxError: Rest parameter must be last formal parameter
 
-
 x1 = (param1, paramn, ...rest ) => {
     console.log(`%c {statements}`, `color: red;`);
 }
-
-
 
 x1 = (param1, paramn) => {
     console.log(`%c {statements}`, `color: red;`);
---
+++ Front-End-Tools/Webpack/node.js-async-generator-promise.js
@@ -0,0 +1,7 @@
+/**
+ * node.js-async-generator-promise.js
+ * 
+ * https://gist.github.com/xgqfrms-GitHub/79f3fcc2e19d1bcbcddb2faa6e362d76
+ *
+ * 
+ */
