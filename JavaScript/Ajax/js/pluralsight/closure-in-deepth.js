347c4a13030727f6185a07c4d9bd57efe139b96e
xgqfrms <xgqfrms@outlook.com>
2017-06-16 00:25:07 +0800

    逗号运算符 & Comma operator
    https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Comma_Operator

--- JavaScript/Ajax/js/pluralsight/closure-in-deepth.js
+++ JavaScript/Ajax/js/pluralsight/closure-in-deepth.js
@@ -104,7 +104,74 @@ closureDate.closure();
 
 
 
+function ClosureDate(){
+    var data = new Date(),
+        closure = function(){
+            return data.getMilliseconds();
+        };
+    console.log(`\ndata = \t`,data.getMilliseconds());
+    return {
+        jsonClosure: closure
+    };
+}
+
+var closureDate = new ClosureDate();
+
+closureDate.jsonClosure();
+
+
+// comma separate ,,,
+
+/*
+
+
+js  ， comma
+
+https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Operators/Comma_Operator
+
+
+https://stackoverflow.com/questions/9579546/when-is-the-comma-operator-useful
+
+https://stackoverflow.com/questions/3561043/what-does-a-comma-do-in-javascript-expressions
+
+逗号运算符评估其操作数（从左到右），并返回最后一个操作数的值。
+
+逗号运算符评估每个操作数（从左到右），并返回最后一个操作数的值。
 
 
 
 
+https://javascriptweblog.wordpress.com/2011/04/04/the-javascript-comma-operator/
+
+
+http://www.jstips.co/en/javascript/comma-operaton-in-js/
+
+https://gist.github.com/isaacs/357981
+
+
+
+*/
+
+
+// standard style
+var a = "ape",
+    b = "bat",
+    c = "cat",
+    d = "dog",
+    e = "elf",
+    f = "fly",
+    g = "gnu",
+    h = "hat",
+    i = "ibu";
+
+// comma-first style
+var a = "ape"
+    , b = "bat"
+    , c = "cat"
+    , d = "dog"
+    , e = "elf"
+    , f = "fly"
+    , g = "gnu"
+    , h = "hat"
+    , i = "ibu"
+    ;
