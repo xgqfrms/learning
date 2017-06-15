23f9ee9a27d102d531541ec1a4fb0793ce8a2c3f
xgqfrms <xgqfrms@outlook.com>
2017-06-16 02:06:42 +0800

    namespace prototype

---
+++ JavaScript/Ajax/js/pluralsight/namespace-prototype.js
@@ -0,0 +1,74 @@
+// prototype namespace
+
+
+var Calculator = function(id) {
+    this.selector = document.querrySelector(id);
+};
+
+
+Calculator.prototype = {
+    add: function(x.y){
+        var val = x + y;
+        this.selector.innerHTML = val;
+        // maybe this caller bugs!
+    }
+};
+
+
+var calc = new Calculator('#id');
+
+calc.add(1,2);
+
+
+Calculator.prototype = {
+    var that = this;
+    add: function(x, y){
+        var val = x + y;
+        that.selector.innerHTML = val;
+        // maybe this caller bugs!
+    }
+};
+
+
+
+// overwrite for libs, without change the sources codes
+
+
+
+Calculator.prototype.add = function(x, y, z){
+    var x = x ? x : 0,
+        y = y ? y : 0,
+        z = z ? z : 0,
+        sum = x + y + z;
+    console.log(`\nsum = \n${sum}`);
+}
+
+
+//  ES6 
+Calculator.prototype.add = (x = 0, y = 0, z = 0) => {
+    let sum = x + y + z;
+    console.log(`\nsum = \n${sum}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
