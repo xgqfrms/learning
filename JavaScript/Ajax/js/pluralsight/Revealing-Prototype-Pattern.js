1126c03be5bd7519291dcd3424feadf36b7e3e69
xgqfrms <xgqfrms@outlook.com>
2017-06-18 01:22:37 +0800

    utf-basic, &, #, 字符  实体  十进制 十六进制
    // 字符  十进制 十六进制    实体  名称
    
    http://www.runoob.com/charsets/ref-utf-basic-latin.html
    
    字符  十进制 十六进制    实体  名称
    
    #   &#35;  &#x0023;       数字符号（NUMBER SIGN）
    $   &#36;  &#x0024;        美元符号（DOLLAR SIGN）
    
    &   &#38;  &#x0026;    &amp;   & 符号（AMPERSAND）
    
    字符  实体  十进制 十六进制    描述
    •   &bull;  &#8226; &#x2022;    着重号 = 黑色小圆点（bullet = black small circle）

--- JavaScript/Ajax/js/pluralsight/Revealing-Prototype-Pattern.js
+++ JavaScript/Ajax/js/pluralsight/Revealing-Prototype-Pattern.js
@@ -11,6 +11,8 @@ http://www.w3school.com.cn/tags/html_ref_entities.html
 
 
 
+
+
 &%26;
 &%23;
 
@@ -20,6 +22,58 @@ u%23;
 
 https://www.w3schools.com/html/html_entities.asp
 
+
+https://www.w3schools.com/html/html_symbols.asp
+
+
+
+<p>I will display &euro;</p>
+<p>I will display &#8364;</p>
+<p>I will display &#x20AC;</p>
+
+
+I will display €
+
+
+
+https://www.w3schools.com/html/html_symbols.asp
+
+
+
+https://www.w3schools.com/charsets/ref_utf_arrows.asp
+
+
+https://www.w3schools.com/charsets/ref_utf_currency.asp
+
+
+https://www.w3schools.com/charsets/ref_utf_symbols.asp
+
+
+https://www.w3schools.com/charsets/ref_utf_greek.asp
+
+
+https://www.w3schools.com/charsets/ref_utf_math.asp
+
+
+// 字符  十进制 十六进制    实体  名称
+
+
+
+
+
+http://www.runoob.com/charsets/ref-utf-basic-latin.html
+
+
+字符  十进制 十六进制    实体  名称
+
+#   &#35;  &#x0023;       数字符号（NUMBER SIGN）
+$   &#36;  &#x0024;        美元符号（DOLLAR SIGN）
+
+&   &#38;  &#x0026;    &amp;   & 符号（AMPERSAND）
+
+
+字符  实体  十进制 十六进制    描述
+•   &bull;  &#8226; &#x2022;    着重号 = 黑色小圆点（bullet = black small circle）
 
 
 
