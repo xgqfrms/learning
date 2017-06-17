4a087792770d58ac48ae6d74db2a5e5f98868b5d
xgqfrms <xgqfrms@outlook.com>
2017-06-18 01:06:56 +0800

    url encode

--- JavaScript/Ajax/js/pluralsight/Revealing-Prototype-Pattern.js
+++ JavaScript/Ajax/js/pluralsight/Revealing-Prototype-Pattern.js
@@ -18,8 +18,31 @@ http://www.w3school.com.cn/tags/html_ref_entities.html
 u%26;
 u%23;
 
+https://www.w3schools.com/html/html_entities.asp
+
+
+
 
 https://html5.komplett.cc/code/index_en.html
+
+
+https://www.w3schools.com/
+
+https://www.w3schools.com/html/default.asp
+
+https://www.w3schools.com/html/html_layout.asp
+
+
+https://www.w3schools.com/tags/ref_keyboardshortcuts.asp
+
+https://www.w3schools.com/html/html5_syntax.asp
+
+
+https://www.w3schools.com/html/html_urlencode.asp
+
+https://www.w3schools.com/tags/ref_urlencode.asp
+
+
 
 */
 
